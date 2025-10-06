045b9943cdea6b46bc0f4f4fdfb11beca4b6f6c4
jjuuny2008 <62197580+jjuuny2008@users.noreply.github.com>
2025-10-06 19:55:55 +0900

    Now copper wire have direction

--- src/main/java/net/devs/electromod/block/custom/electro/CopperWire.java
+++ src/main/java/net/devs/electromod/block/custom/electro/CopperWire.java
@@ -1,11 +1,17 @@
 package net.devs.electromod.block.custom.electro;
 
 
+import com.mojang.serialization.MapCodec;
+import net.devs.electromod.block.custom.magnetic.CopperCoilBlock;
 import net.devs.electromod.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -14,10 +20,17 @@ import net.minecraft.world.World;
 
 public class CopperWire extends Block {
 
+    public static final MapCodec<CopperWire> CODEC = CopperWire.createCodec(CopperWire::new);
+    public static final DirectionProperty FACING = Properties.FACING;
 
     public CopperWire(Settings settings)
     {
         super(settings);
+    }
+
+    @Override
+    protected MapCodec<? extends Block> getCodec() {
+        return super.getCodec();
     }
 
     @Override
@@ -36,5 +49,15 @@ public class CopperWire extends Block {
         super.onSteppedOn(world, pos, state, entity);
 
         entity.kill();
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
     }
 }
