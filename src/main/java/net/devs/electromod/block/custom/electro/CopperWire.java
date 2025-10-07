40df5366367e7b79bcd2a2439843791be1c1b3c0
jjuuny2008 <62197580+jjuuny2008@users.noreply.github.com>
2025-10-07 13:08:29 +0900

    Added Lightning to CooperWire

--- src/main/java/net/devs/electromod/block/custom/electro/CopperWire.java
+++ src/main/java/net/devs/electromod/block/custom/electro/CopperWire.java
@@ -26,7 +26,6 @@ import net.minecraft.world.WorldAccess;
 public class CopperWire extends Block {
 
     public static final DirectionProperty FACING = Properties.FACING;
-
     public static final BooleanProperty NORTH = Properties.NORTH;
     public static final BooleanProperty EAST = Properties.EAST;
     public static final BooleanProperty SOUTH = Properties.SOUTH;
@@ -47,6 +46,7 @@ public class CopperWire extends Block {
         return super.getCodec();
     }
 
+    // ⚡ 우클릭 감전 이벤트
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (world.isClient()) return ActionResult.FAIL;
@@ -58,13 +58,14 @@ public class CopperWire extends Block {
             lightning.refreshPositionAndAngles(blockpos.getX(), blockpos.getY(), blockpos.getZ(), 0, 0);
 
             world.spawnEntity(lightning);
-
             player.kill();
             player.sendMessage(Text.literal("Oops!"));
         }
+
         return ActionResult.SUCCESS;
     }
 
+    // ⚡ 밟았을 때 감전 이벤트
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (world.isClient()) return;
@@ -75,7 +76,6 @@ public class CopperWire extends Block {
         lightning.refreshPositionAndAngles(blockpos.getX(), blockpos.getY(), blockpos.getZ(), 0, 0);
 
         world.spawnEntity(lightning);
-
         entity.kill();
     }
 
@@ -90,61 +90,21 @@ public class CopperWire extends Block {
     }
 
     @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        if (world.isClient()) return;
+    protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify)
+    {
+        //좌표 기준으로 블록 불러오기
+        Block EAST = world.getBlockState(new BlockPos(pos.getX()+1, pos.getY() ,pos.getZ())).getBlock();
+        Block WEST = world.getBlockState(new BlockPos(pos.getX()-1, pos.getY() ,pos.getZ())).getBlock();
+        Block SOUTH = world.getBlockState(new BlockPos(pos.getX(), pos.getY() ,pos.getZ()+1)).getBlock();
+        Block NORTH = world.getBlockState(new BlockPos(pos.getX(), pos.getY() ,pos.getZ()-1)).getBlock();
 
-        // 단순화: 항상 월드의 north/east/south/west 검사
-        BlockState newState = state
-                .with(NORTH, world.getBlockState(pos.north()).isOf(this))
-                .with(EAST,  world.getBlockState(pos.east()).isOf(this))
-                .with(SOUTH, world.getBlockState(pos.south()).isOf(this))
-                .with(WEST,  world.getBlockState(pos.west()).isOf(this));
+        //CopperWire인지 확인하기
+        if(EAST.getName() != this.getName()) {EAST = null;}
+        if(WEST.getName() != this.getName()) {WEST = null;}
+        if(SOUTH.getName() != this.getName()) {SOUTH = null;}
+        if(NORTH.getName() != this.getName()) {NORTH = null;}
 
-        if (!newState.equals(state)) {
-            world.setBlockState(pos, newState, 3);
-            state = newState;
-        }
 
-        // 주변 동일 블록들의 해당(반대) 방향 프로퍼티를 true로 설정 (양방향 연결)
-        for (Direction dir : Direction.values()) {
-            if (!dir.getAxis().isHorizontal()) continue;
 
-            BlockPos nPos = pos.offset(dir);
-            BlockState nState = world.getBlockState(nPos);
-
-            if (!nState.isOf(this)) continue;
-
-            // neighbor가 우리를 바라보는 방향의 프로퍼티를 가져온다 (반대 방향)
-            BooleanProperty neighborProp = getPropertyFor(dir.getOpposite());
-            if (!nState.get(neighborProp)) {
-                BlockState updated = nState.with(neighborProp, true);
-                world.setBlockState(nPos, updated, 3);
-            }
-        }
-
-    }
-
-    @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        // direction: world direction FROM this block TO neighbor
-        if (direction.getAxis().isHorizontal()) {
-            BooleanProperty prop = getPropertyFor(direction);
-            boolean connect = world.getBlockState(neighborPos).isOf(this);
-            return state.with(prop, connect);
-        }
-        return state;
-    }
-
-    // 방향 -> BooleanProperty 매핑 헬퍼
-    private static BooleanProperty getPropertyFor(Direction direction) {
-        return switch (direction) {
-            case NORTH -> NORTH;
-            case EAST  -> EAST;
-            case SOUTH -> SOUTH;
-            case WEST  -> WEST;
-            default    -> throw new IllegalArgumentException("Unsupported direction: " + direction);
-        };
     }
 }
-
-
