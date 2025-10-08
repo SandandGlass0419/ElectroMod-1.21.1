7c75c7b80202cda80bd9beee2ee7b5e6912e5dbd
jjuuny2008 <62197580+jjuuny2008@users.noreply.github.com>
2025-10-08 17:54:21 +0900

    NowBattery provides 10f of electrocity

--- src/main/java/net/devs/electromod/block/custom/electro/Battery.java
+++ src/main/java/net/devs/electromod/block/custom/electro/Battery.java
@@ -1,5 +1,6 @@
 package net.devs.electromod.block.custom.electro;
 
+import net.devs.electromod.block.entity.custom.electro.WireBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemPlacementContext;
@@ -10,17 +11,18 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 
 public class Battery extends Block {
     public static final DirectionProperty FACING = Properties.FACING;
 
-    public Battery(Settings settings)
-    {
+    public Battery(Settings settings) {
         super(settings);
         this.setDefaultState(this.stateManager.getDefaultState()
                 .with(FACING, Direction.UP));
     }
 
+    // 설치 방향 설정: 플레이어가 바라보는 방향을 앞면으로
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
@@ -31,8 +33,29 @@ public class Battery extends Block {
         builder.add(FACING);
     }
 
+
+
+    @Override
+    protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        super.onBlockAdded(state, world, pos, oldState, notify);
+
+        // 1틱 후 scheduledTick 호출
+        world.scheduleBlockTick(pos, this, 1);
+    }
+
+    // scheduledTick: FACING 방향에 WireBlock 있으면 전류 1로 설정
     @Override
     protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         super.scheduledTick(state, world, pos, random);
+
+        Direction facing = state.get(FACING);
+        BlockPos targetPos = pos.offset(facing);
+        BlockState targetState = world.getBlockState(targetPos);
+
+        if (targetState.getBlock() instanceof net.devs.electromod.block.custom.electro.WireBlock) {
+            if (world.getBlockEntity(targetPos) instanceof WireBlockEntity wireBE) {
+                wireBE.setElectrocity(10f); // 전류 1로 설정
+            }
+        }
     }
 }
--- src/main/java/net/devs/electromod/block/entity/custom/electro/WireBlockEntity.java
+++ src/main/java/net/devs/electromod/block/entity/custom/electro/WireBlockEntity.java
@@ -10,7 +10,6 @@ import net.minecraft.util.math.BlockPos;
 public class WireBlockEntity extends BlockEntity {
 
     private float Electrocity = 0; // 여기에 큰 값 저장 가능
-    private float voltage = 0;
 
     public WireBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.WIRE_BE, pos, state);
@@ -28,29 +27,16 @@ public class WireBlockEntity extends BlockEntity {
         return Electrocity;
     }
 
-    public void addVoltage(float value) {
-        this.voltage += value;
-        markDirty(); // 값 변경 후 월드에 반영
-    }
-
-    // 월드 저장용 NBT
-    public float getVoltage() {
-        return voltage;
-    }
-
-
-
-
 
     @Override
     protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.readNbt(nbt, registryLookup);
-        this.Electrocity = nbt.getInt("StoredValue");
+        this.Electrocity = nbt.getInt("Electrocity");
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.writeNbt(nbt, registryLookup);
-        nbt.putFloat("StoredValue", Electrocity);
+        nbt.putFloat("Electrocity", Electrocity);
     }
 }
