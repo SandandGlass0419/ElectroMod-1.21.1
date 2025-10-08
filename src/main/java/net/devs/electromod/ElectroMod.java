59ddc637a9abbd96ca959b9ad807a54886b1d2e0
SandandGlass <sandandglass0419@gmail.com>
2025-10-09 02:11:03 +0900

    Added tests

--- src/main/java/net/devs/electromod/ElectroMod.java
+++ src/main/java/net/devs/electromod/ElectroMod.java
@@ -1,13 +1,17 @@
 package net.devs.electromod;
 
 import net.devs.electromod.block.ModBlocks;
+import net.devs.electromod.block.custom.magnetic.MagneticForce.MVec3i;
 import net.devs.electromod.block.entity.ModBlockEntities;
 import net.devs.electromod.components.ModDataComponentTypes;
 import net.devs.electromod.item.ModItemGroups;
 import net.devs.electromod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.math.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
 
 public class ElectroMod implements ModInitializer
 {
@@ -22,5 +26,35 @@ public class ElectroMod implements ModInitializer
         ModItemGroups.registerModItemGroups();
         ModDataComponentTypes.registerModDataComponentTypes();
         ModBlockEntities.registerModBlockEntities();
+        //Test();
 	}
+
+    private static void Test()
+    {
+       testRotation();
+       testSetRotation();
+    }
+
+    private static void testRotation()
+    {
+        MVec3i mVec3i = new MVec3i(1, -1, 2, Direction.UP);
+        var res = mVec3i.rotate90(Direction.Axis.Z, MVec3i.Angles.CLOCK_270.ordinal());
+        ElectroMod.LOGGER.info("result: " + res.getX() + res.getY() + res.getZ() + res.getForceDirection().toString());
+    }
+
+    private static void testSetRotation()
+    {
+        Set<MVec3i> set = Set.of(new MVec3i(-1, 0, 0, Direction.WEST),
+                                 new MVec3i(-1, 0, -1, Direction.NORTH),
+                                 new MVec3i(0, 0, -1, Direction.EAST),
+                                 new MVec3i(1, 0, -1, Direction.SOUTH),
+                                 new MVec3i(1, 0, 0, Direction.WEST));
+
+        var res = MVec3i.rotate90(set, Direction.Axis.Y, MVec3i.Angles.CLOCK_270.ordinal());
+
+        for (MVec3i vec : res)
+        {
+            ElectroMod.LOGGER.info("result: " + vec.getX() + vec.getY() + vec.getZ() + vec.getForceDirection().toString());
+        }
+    }
 }
